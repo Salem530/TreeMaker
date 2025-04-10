bc84ccc0686d374e6c9e93dd1eade319c678cf37
Salem Malola <salemmalola530@gmail.com>
2025-04-10 22:26:36 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,9 @@
+/*
+    TreeMaker console application to create tree structure from a trm file. 
+*/ 
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("Hello word !");
+    return 0;
+}
