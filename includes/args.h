9555818e06a8925141775bb75c1eea2034402952
Salem Malola <salemmalola530@gmail.com>
2025-05-16 09:09:36 +0100

    feat (args) : create the args management

--- includes/args.h
+++ includes/args.h
@@ -1,6 +1,16 @@
 #ifndef __ARGS_H__
 #define __ARGS_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct {
+    const char *input_file;
+    _Bool debug_mode;
+} Args;
+
+int parse_args(Args *args);
+int print_help();
 
 #endif
--- src/args.c
+++ src/args.c
@@ -1,1 +1,26 @@
 #include "args.h"
+
+int parse_args(Args *args){
+    args->input_file = NULL;
+    args->debug_mode = 0;
+    for(int i = 1; i < __argc; i++){
+        if(!strcmp(__argv[i], "--help")){
+            print_help();
+            return 0;
+        }
+        else if(!strcmp(__argv[i], "--debug") || !strcmp(__argv[i], "-d"))
+            args->debug_mode = 1;
+        else if((!strcmp(__argv[i], "--tree") || !strcmp(__argv[i], "-d")) && i+1 < __argc)
+            args->input_file = __argv[i+1];
+        else {
+            fprint(stderr, "Unknown argument %s\n", __argv[i]);
+            return 0;
+        }
+    }
+
+    return args->input_file != NULL;
+}
+
+void print_help(){
+    printf("Create tree templates from tree file.\n\ntreemaker [options] --tree, -t [input_file]\n\n--debug, -d\tActive the debug mode.\n\n--tree, -t\tThe input file to create the project tree.\n\n");
+}
