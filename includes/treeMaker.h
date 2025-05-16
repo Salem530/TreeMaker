90f6dc4a526f7fb8cf8cae3dfcde7aa0ce9b8cf4
Salem Malola <salemmalola530@gmail.com>
2025-05-16 08:23:28 +0100

    feat (treeMaker) : create the tree node class

--- includes/treeMaker.h
+++ includes/treeMaker.h
@@ -1,6 +1,11 @@
 #ifndef __TREEMAKER_H__
 #define __TREEMAKER_H__
 
-
+typedef struct TreeNode {
+    char *name;
+    _Bool is_directory;
+    struct TreNode **children;
+    int child_count;
+} TreeNode;
 
 #endif
